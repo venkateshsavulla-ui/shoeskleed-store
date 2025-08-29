d87c28b52f24e763df15f864037bb99f2202cae2
venkateshsavulla-ui <venkateshsavulla@gmail.com>
2025-08-29 15:25:59 +0530

    Add files via upload
    common changes

--- _app.js
+++ _app.js
@@ -1,17 +1,14 @@
-import { CartProvider } from "react-use-cart";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import AuthModal from "../components/AuthModal";
-import "../styles/globals.css";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <CartProvider>
+    <>
       <Header />
-      <AuthModal />   {/* 👈 this makes modal appear on ALL pages */}
       <Component {...pageProps} />
       <Footer />
-    </CartProvider>
+    </>
   );
 }
 
